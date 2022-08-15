53c3096a7be898e127fba9a787f99d4005550a99
coding-priest7 <sandeep17goswami@gmail.com>
2022-08-16 00:25:01 +0530

    updated commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const FooterDiv = styled.div`
 export function Footer() {
   return (
     <FooterDiv>
-      Made with ♥ by&nbsp;<a href="https://github.com/sadanandpai/">Sadanand Akshay Pai</a>
+      Made with ♥ by&nbsp;<a href="https://github.com/coding-priest7">Sandeep Goswami</a>
     </FooterDiv>
   );
 }
